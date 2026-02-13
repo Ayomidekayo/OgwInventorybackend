4a76f95bd980822e36e34faa6926e361d02963cd
YourName <your-email@example.com>
2026-02-13 08:53:36 +0100

    perfect url added

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(
       const allowedOrigins = [
         "http://localhost:5173",
         "https://ogwfrontend.vercel.app",
-        "https://ogwfrontend-rgfs-anbf8w46d-movie-apps-projects-4b57ba93.vercel.app",
+        "https://ogwinventorysystem.vercel.app",
         "https://ogwfrontend-d4ga-5w5brc4tx-movie-apps-projects-4b57ba93.vercel.app"
       ];
 
